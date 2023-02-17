c17ef6e5c0a3c7bc1e5ecdedbbeef623935b43b1
arfla <46490532+arfla@users.noreply.github.com>
2023-02-17 22:45:19 +0000

    plot wrapper

--- src/views/awesomeSink.tsx
+++ src/views/awesomeSink.tsx
@@ -4,11 +4,18 @@ import axios from "axios";
 import { SettingsContext } from "../context/Settings";
 import { factoryRefs } from "../constants/buildings";
 import { itemRefs } from "../constants/items";
+import PlotFigure from "./test";
+// @ts-ignore
+import * as Plot from "@observablehq/plot";
+// import { data } from "../constants/test-data";
 
 export const AwesomeSink:React.FC = (props) => {
     
     const [doLoadData, setLoadData] = useState(true);
     const [sinks, setSink] = useState<undefined | any>(undefined);
+    const [sinks2, setSink2] = useState<undefined | any>(undefined);
+
+    const [data, setData] = useState(undefined)
 
     const settings = useContext(SettingsContext);
 
@@ -16,14 +23,20 @@ export const AwesomeSink:React.FC = (props) => {
         if (doLoadData === true) {
 
             const response = await axios.get("http://127.0.0.1:8080/getResourceSink");
+            const response2 = await axios.get("http://127.0.0.1:8080/getExplorationSink");
             setSink(response.data);
-            console.log(response.data);
+            setSink2(response2.data);
+            //console.log(response.data);
 
             setTimeout(() => {
                 loadData();
             }, settings.msInterval);
         }
     };
+
+    useEffect(()=>{
+        if(sinks2) setData(sinks2[0].GraphPoints);
+    }, [sinks2])
     
     useEffect(()=>{
         loadData();
@@ -31,7 +44,7 @@ export const AwesomeSink:React.FC = (props) => {
 
     return(
         <Container sx={{paddingTop:'50px'}}>
-            {sinks ? 
+            {sinks && sinks2 ?
                 <>
                     <Grid container spacing={3} sx={{marginBottomY: '30px'}} display={'flex'} alignItems={'center'}>
                         {sinks.map((sink:any, index:number)=>{
@@ -45,12 +58,14 @@ export const AwesomeSink:React.FC = (props) => {
                                                     <Box sx={{marginBottom: '10px'}}>
                                                         <Stack alignItems={"center"}>
                                                             <img src={"./assets/"+factoryRefs["Awesome Sink"].image} alt="image" style={{height: '70px', width: '70px'}}></img>
-                                                            <Typography level="h6" marginY={'15px'}>{sink.Name}</Typography>
+                                                            <Typography level="h6" marginY={'15px'}>{sinks2[0].Name}</Typography>
                                                             <Box sx={{display:'flex', flexDirection: 'row', alignItems: 'center'}}>
-                                                                <Typography level="h6">{sink.NumCoupon}</Typography> 
+                                                                <Typography level="h6">{sinks2[0].NumCoupon}</Typography> 
                                                                 <img src={"./assets/"+itemRefs["FICSIT Coupon"].image} alt="image" style={{height: '35px', width: '35px'}}></img>
                                                             </Box>
-                                                            <Typography level="h6">{sink.NumCoupon}</Typography>
+                                                            <Typography level="h6">{Math.floor(sinks2[0].Percent)}%</Typography>
+                                                            <Typography level="h6">Next Coupon: {sinks2[0].PointsToCoupon}</Typography>
+                                                            <Typography level="h6">Total Points: {sinks2[0].TotalPoints}</Typography>
                                                         </Stack>  
 
                                                     </Box>
@@ -61,7 +76,49 @@ export const AwesomeSink:React.FC = (props) => {
                                 </Grid>
                             )
                         })}
+                        <Grid xs={6}>
+            
+                            <Card  variant="outlined" sx={{position:'relative'}}>
+                                <CardContent>                                                                                
+                                    {data ? <Box sx={{'& svg':{
+                                            background: 'transparent'
+                                        }, color: 'white'}}>
+                                            <PlotFigure options={{
+                                                // width: (dimensions.width-40),
+                                                // height: (dimensions.height-40),
+                                                inset: 20,
+                                                y: {
+                                                    grid: true,
+                                                },
+                                                x: {
+                                                    // line: true,
+                                                },
+                                                color: {
+                                                    domain: [1, 0, -1],                         // Down - Equal - Up
+                                                    range: ["#4daf4a", "#999999", "#e41a1c"],   // Red - Gray - Green
+                                                },
+                                                marks: [
+                                                    Plot.line(data, {
+                                                        x: "Index",
+                                                        y: "value",
+                                                        stroke: 'rgba(255,255,255,1)',
+                                                        strokeWidth: 3,
+                                                        curve: "catmull-rom",
+                                                        marker: "circle",
+                                                        title: (d:any) => `AAPL \n High: ${d.High}` // \n makes a new line
+                                                    })
+                                                ]
+                                            }}></PlotFigure>
+                                        </Box>
+                                    :
+                                        <CircularProgress/>
+                                    }
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     </Grid> 
+
+                    
                 </>
             :
                 <>
---
+++ src/views/plotWrapper.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useEffect, useRef } from "react";
+// @ts-ignore
+import * as Plot from "@observablehq/plot";
+import { addTooltips } from "../components/tooltips";
+
+export type Props = {
+  options: any;
+};
+
+const PlotFigure: FC<Props> = ({ options }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const plot = addTooltips(Plot.plot(options), {});
+    const element = ref.current;
+    if (element) {
+      const child = element.children[0];
+      if (child) {
+        child.remove();
+      }
+      element.appendChild(plot);
+    }
+  }, [ref, options]);
+
+  return <div style={{backgroundColor: 'transparent'}} ref={ref} />;
+};
+
+export default PlotFigure;
