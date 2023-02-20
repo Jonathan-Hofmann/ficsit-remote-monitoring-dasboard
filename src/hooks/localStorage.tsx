748d305c0d0a5fca04d943d9173d9e4620800f92
Jonathan Hofmann <jonawebdesign@gmail.com>
2023-02-20 01:04:42 +0100

    updated settings and localStorage Hook

--- src/hooks/localStorage.tsx
+++ src/hooks/localStorage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const useLocalStorage = (storageKey:string, fallbackState:string[]) => {
+export const useLocalStorage = (storageKey:string, fallbackState:any) => {
     const [value, setValue] = React.useState(
-      JSON.parse(localStorage.getItem(storageKey) as string) ?? ["Iron Ingot", "Iron Plates", "Iron Rods", "Copper Ingot", "Concrete", "Coal"]
+      JSON.parse(localStorage.getItem(storageKey) as string) ?? fallbackState
     );
   
     React.useEffect(() => {
