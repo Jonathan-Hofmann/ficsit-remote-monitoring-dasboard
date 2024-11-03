53f936a4527496e3ca3982532624968f4b891165
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-11-03 23:23:04 +0100

    Delete useless create-react-app files and combine index.tsx and app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createTheme } from '@mui/material/styles';
-
+import { BrowserRouter } from 'react-router-dom';
+import { MainWrapper } from './components/mainWrapper';
+import { ThemeWrapper } from './components/themeWrapper';
+import { ToggleTheme } from './components/toggleTheme';
 
 
 const root = ReactDOM.createRoot(
@@ -12,12 +12,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeWrapper>
+      <ToggleTheme>
+        <BrowserRouter>
+          <MainWrapper />
+        </BrowserRouter>
+      </ToggleTheme>
+    </ThemeWrapper>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
