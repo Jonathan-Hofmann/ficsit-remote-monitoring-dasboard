c9e017bf14ee42391490aca2ee0b8afd2c9fdbe3
Jonathan <63954504+Jonathan-Hofmann@users.noreply.github.com>
2024-11-30 23:57:14 +0100

    Added theme, them-toggle and sidebar and some cleaning up and small additions...

---
+++ src/app/overview/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import type React from "react";
+
+const Homepage = (): React.JSX.Element => {
+  return (
+    <div className="container pt-8 mx-auto">
+      <h1 className="text-4xl font-bold">Welcome back Pioneer!</h1>
+    </div>
+  );
+};
+
+export default Homepage;
